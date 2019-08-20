9f4d46d045a124e0037022ea1819a4f3e416c45b
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-08-20 17:21:55 +0530

    Update studentDatabase.cpp

--- studentDatabase/studentDatabase.cpp
+++ studentDatabase/studentDatabase.cpp
@@ -1,110 +1,104 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
 class student
 {
-
 public:
 	int rollno;
-	std::string name, div;
-
-	void accept(int x)
+	string name, div;
+	void accept(int srno)
 	{
-		cout << "\nStudent number " << x << ":\nEnter roll no.: ";
+		cout << "\nStudent number " << srno << ":\nEnter roll no.: ";
 		cin >> rollno;
 		cout << "Enter name: ";
 		cin >> name;
 		cout << "Enter division: ";
 		cin >> div;
 	}
-
-	void display(int x)
+	void display(int srno)
 	{
-		cout << x << "\t" << this->rollno << "\t\t" << this->name << "\t" << this->div << endl;
+		cout << srno << "\t" << this->rollno << "\t\t" << this->name << "\t" << this->div << endl;
 	}
-
-	void modify(int x)
+	void modify(int srno)
 	{
-		cout << "\nModyfing details of Student no. " << x << "\n Current details: Roll No.: " << this->rollno << "\nName: " << this->name << "\nDivison: " << this->div << "\nEnter new details::\nRoll no.: ";
+		cout << "\nModyfing details of Student no. " << srno << "\n Current details: Roll No.: " << this->rollno << "\nName: " << this->name << "\nDivison: " << this->div << "\nEnter new details::\nRoll no.: ";
 		cin >> this->rollno;
 		cout << "Name: ";
 		cin >> this->name;
 		cout << "Division: ";
 		cin >> this->div;
 	}
-
-	void del(int x)
-	{
-		this->rollno = 0;
-		this->name = "";
-		this->div = "";
-		cout << "Sr. No. " << x << " deleted";
-	}
+	// void del(int srno)
+	// {
+	// 	this->rollno = 0;
+	// 	this->name = "";
+	// 	this->div = "";
+	// 	cout << "Sr. No. " << srno << " deleted";
+	// }
 };
-
+void display(student s[], int currentValue)
+{
+	cout << "\nSr. No.\tRoll no.\tName\tDivison" << endl;
+	for (int i = 0; i < currentValue; i++)
+		s[i].display(i + 1);
+}
+int del(student s[], int currentValue, int srno)
+{
+	currentValue--;
+	for (int i = srno - 1; i < currentValue; i++)
+		s[i] = s[i + 1];
+	return currentValue;
+}
 int main()
 {
-	int n, c = 1, x;
-	bool d = true;
-	cout << "Enter the total number of students: ";
+	int n, choice = 1, srno, currentValue;
+	cout << "Enter the maximum size of student database (maximum number of sr. nos.): ";
 	cin >> n;
 	student s[n];
-	for (int i = 0; i < n; i++)
-		s[i].accept(i + 1);
-	while (c != 0)
+	while (choice != 0)
 	{
-		switch (c)
+		cout << "\nEnter\n1.to add student\n2.to display database\n3.to modify data\n4.to delete data\n0.to exit\nInput: ";
+		cin >> choice;
+		switch (choice)
 		{
+		case 0:
+			cout << "\nThank You!!\n";
+			break;
 		case 1:
-			if (!d)
-				cout << "Updated databse:\n";
-			cout << "\nSr. No.\tRoll no.\tName\tDivison" << endl;
-			for (int i = 0; i < n; i++)
-				s[i].display(i + 1);
-			d = true;
+			s[currentValue].accept(currentValue + 1);
 			break;
 		case 2:
-			x = 0;
-			cout << "Enter the Sr. no. :";
-			while (x <= 0 || x > n)
-			{
-				cin >> x;
-				if (x <= 0 || x > n)
-					cout << "Invalid value!!";
-				else
-					s[x - 1].modify(x);
-			}
-			d = false;
-			c = 1;
+			cout << "Updated databse:\n";
+			display(s, currentValue);
 			break;
 		case 3:
-			x = 0;
+			srno = 0;
 			cout << "Enter the Sr. no. :";
-			while (x <= 0 || x > n)
+			while (srno <= 0 || srno > currentValue)
 			{
-				cin >> x;
-				if (x <= 0 || x > n)
+				cin >> srno;
+				if (srno <= 0 || srno > currentValue)
 					cout << "Invalid value!!";
 				else
-					s[x - 1].del(x);
+					s[srno - 1].modify(srno);
 			}
-			d = false;
-			c = 1;
+			break;
+		case 4:
+			srno = 0;
+			cout << "Enter the Sr. no. :";
+			while (srno <= 0 || srno > n)
+			{
+				cin >> srno;
+				if (srno <= 0 || srno > currentValue)
+					cout << "Invalid value!!";
+				else
+					currentValue = del(s, currentValue, srno);
+			}
 			break;
 		default:
 			cout << "Invalid input!!!";
 			break;
 		}
-		if (d)
-		{
-			cout << "\nEnter\n2.to display database\n3.to modify data\n4.to delete data\n0.to exit\nInput: ";
-			cin >> c;
-			if (c == 0)
-				cout << "\nThank You!!\n";
-		}
 	}
-
 	return 0;
 }
