5fe74d73dc20027bbfb12a92526ef39fd10349d0
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-10-22 23:21:18 +0530

    Create exceptHandle.java

---
+++ ExceptHandle/exceptHandle.java
@@ -0,0 +1,12 @@
+
+class exceptHandle {
+	public static void main(String[] args) {
+		int i = 10, j = 0, k;
+		try {
+			k = i / j;
+			System.out.println(k);
+		} catch (Exception e) {
+			System.out.println("Divide by 0 error");
+		}
+	}
+}
