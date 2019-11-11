076febd48d4001130ca52a546fe524cc8222487f
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-11-11 14:58:59 +0530

    constructor java

---
+++ constructor and exception handeling java/cons.bat
@@ -0,0 +1,1 @@
+javac cons.java && java cons
---
+++ constructor and exception handeling java/cons.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+class cons {
+	public static void main(String[] args) {
+		ca ca = new ca();
+	}
+}
+
+class ca {
+	ca() {
+		out.println("Welcome to constructor of class ca");
+	}
+}	
+/* 
+Output:
+Welcome to constructor of class ca
+*/
