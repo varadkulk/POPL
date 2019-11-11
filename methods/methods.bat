1ad1d94d23f9af5374b485b8a67caee193a52dbe
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-11-11 14:51:32 +0530

    methods

---
+++ methods/methods.bat
@@ -0,0 +1,1 @@
+javac methods.java && java methods
---
+++ methods/methods.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+class methods {
+	static int square(int x) {
+		return x * x;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		out.print("Enter the number: ");
+		int x = Integer.parseInt(sc.nextLine());
+		out.println("Square of " + (x) + " is " + square(x));
+	}
+
+}
